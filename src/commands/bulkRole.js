8b32bce5f9e5a843c2464c734c4a9fbd2c670896
David0403 <48942146+Noboroto@users.noreply.github.com>
2023-08-03 01:34:04 +0700

    Update bulkRole.js

--- src/commands/bulkRole.js
+++ src/commands/bulkRole.js
@@ -5,12 +5,12 @@ module.exports = {
 		.setName("bulk-assign-role")
 		.setDescription("Assign a role for many members by ID")
 		.addStringOption(Option =>
-			Option.setName("IDs")
+			Option.setName("ids")
 				.setDescription("seperate by comma, can be present as mention")
 				.setRequired(true),
 		)
 		.addRoleOption(Option =>
-			Option.setName("Role")
+			Option.setName("role")
 				.setDescription("Role you want to assign")
 				.setRequired(true),
 		)
@@ -20,8 +20,8 @@ module.exports = {
 		// interaction.user is the object representing the User who ran the command
 		// interaction.member is the GuildMember object, which represents the user in the specific guild
 		const guild = interaction.guild;
-		const role = interaction.options.getRole("Role");
-		const ids = interaction.options.getString("IDs").replace(">", "").replace("<", "").replace("@").split(",");
+		const role = interaction.options.getRole("role");
+		const ids = interaction.options.getString("ids").replace(">", "").replace("<", "").replace("@").split(",");
 
 		ids.forEach(id => {
 			const member = guild.members.cache.get(id);
