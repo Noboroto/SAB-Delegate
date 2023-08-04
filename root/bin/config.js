82fcec19e125f59e25192a2a344ffb2e8158e981
David0403 <48942146+Noboroto@users.noreply.github.com>
2023-08-05 01:27:52 +0700

    Remove useless

--- root/bin/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    token: "MTA0MDgwMDQ2OTI5NzE1NjExNg.Ggm-rd.9DF2DttTC5VekLqoG0-ilauviSjcAkq59Nvq1c",
-    clientId: "1040800469297156116",
-    guildId: "713025650176294945"
-};
-//# sourceMappingURL=config.js.map
