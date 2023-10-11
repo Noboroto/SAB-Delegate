888d5e98ac4104c30b01c93a86db3119299d06f2
David0403 <48942146+Noboroto@users.noreply.github.com>
2023-10-11 23:48:38 +0700

    Update bulkRole.ts

--- root/src/commands/bulkRole.ts
+++ root/src/commands/bulkRole.ts
@@ -54,8 +54,8 @@ export default {
 			if (!member) return;
 			counter++;
 			console.log(counter);
-			//if (!isRemove) await member.roles.add(role);
-			//else await member.roles.remove(role);
+			if (!isRemove) await member.roles.add(role);
+			else await member.roles.remove(role);
 		})
 
 		console.log(counter + " members done");
