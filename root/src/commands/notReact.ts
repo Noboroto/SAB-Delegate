a1906dd10477413fc17ecf0e98a403719df4687d
David0403 <48942146+Noboroto@users.noreply.github.com>
2023-10-21 10:47:00 +0700

    Update notReact.ts

--- root/src/commands/notReact.ts
+++ root/src/commands/notReact.ts
@@ -22,12 +22,21 @@ export default {
 				.setRequired(true)
 		)
 
+		.addBooleanOption((Option) =>
+			Option.setName("is-reply")
+				.setDescription("default is false")
+				.setRequired(false)
+		)
+
 		.setDefaultMemberPermissions(PermissionFlagsBits.ViewChannel)
 		.setDMPermission(false),
 
 	async execute(interaction: ChatInputCommandInteraction) {
 		// interaction.user is the object representing the User who ran the command
 		// interaction.member is the GuildMember object, which represents the user in the specific guild
+
+		const isReply = interaction.options.getBoolean("is-reply");
+
 		const messageFromID = await getMessageFromOption(
 			interaction,
 			"message-link"
@@ -35,7 +44,6 @@ export default {
 
 		const role = interaction.options.getRole("role") as Role;
 		const members = role.members;
-
 
 		if (!messageFromID) {
 			await interaction.editReply({
@@ -45,18 +53,16 @@ export default {
 		}
 
 		const reactMemberListID: string[] = [];
-		
+
 		const reactionList = await messageFromID.reactions.cache;
 
 		for (const reactionFromMessage of reactionList.values()) {
-			await reactionFromMessage.users
-				.fetch()
-				.then(async (users) => {
-					await users.forEach(async (user) => {
-						if (user.bot) return;
-						reactMemberListID.push(user.id);
-					});
+			await reactionFromMessage.users.fetch().then(async (users) => {
+				await users.forEach(async (user) => {
+					if (user.bot) return;
+					reactMemberListID.push(user.id);
 				});
+			});
 		}
 
 		let replyMsg = `Users who didn't reacted are: \n`;
@@ -64,23 +70,20 @@ export default {
 			(member) =>
 				reactMemberListID.find((id) => id === member.id) === undefined
 		);
-		
 
 		if (absent.size === 0) {
 			interaction.reply({
 				content: "Everyone in role reacted to the message",
 				ephemeral: true,
 			});
-		}
-		else {
+		} else {
 			absent.forEach((member) => {
 				replyMsg += member.toString() + "\n";
 			});
-			messageFromID.reply(replyMsg);
-			interaction.reply({
-				content: "Successfully check the message",
-				ephemeral: true,
-			});
+			if (isReply) {
+				interaction.reply(replyMsg);
+			}
+			else messageFromID.reply(replyMsg);
 		}
 	},
 };
