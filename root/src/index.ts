65a316cdd46813cb5e29cc026b5e3a4992c2873d
David0403 <48942146+Noboroto@users.noreply.github.com>
2023-11-14 21:51:23 +0700

    Update index.ts

--- root/src/index.ts
+++ root/src/index.ts
@@ -4,57 +4,64 @@ import events from "./events-bot/index";
 import config from "./configValues";
 
 // Create a new client instance
-for (const token in config.tokens)
-{
-	const client = new Client({
-		intents: [
-			GatewayIntentBits.Guilds,
-			GatewayIntentBits.GuildMessages,
-			GatewayIntentBits.MessageContent,
-			GatewayIntentBits.GuildMembers,
-			GatewayIntentBits.GuildVoiceStates,
-			GatewayIntentBits.GuildMessageReactions,
-			GatewayIntentBits.GuildMessageTyping,
-			GatewayIntentBits.GuildPresences,
-			GatewayIntentBits.GuildEmojisAndStickers,
-			GatewayIntentBits.MessageContent,
-		],
-	});
 
-	for (const event of events) {
-		if (event.once) {
-			try {
-				client.once(event.name as string, (...args: []) =>
+const startBot = async(token) => {
+		const client = new Client({
+			intents: [
+				GatewayIntentBits.Guilds,
+				GatewayIntentBits.GuildMessages,
+				GatewayIntentBits.MessageContent,
+				GatewayIntentBits.GuildMembers,
+				GatewayIntentBits.GuildVoiceStates,
+				GatewayIntentBits.GuildMessageReactions,
+				GatewayIntentBits.GuildMessageTyping,
+				GatewayIntentBits.GuildPresences,
+				GatewayIntentBits.GuildEmojisAndStickers,
+				GatewayIntentBits.MessageContent,
+			],
+		});
+
+		for (const event of events) {
+			if (event.once) {
+				try {
+					client.once(event.name as string, (...args: []) =>
+						event.execute([...args])
+					);
+				} catch (error) {
+					const channel = client.guilds.cache
+						.get("713025650176294945")
+						.channels.cache.find(
+							(channel) => channel.name === "bot-log"
+						) as TextChannel;
+
+					channel.send({
+						content: `Error executing ${event.name}`,
+					});
+					channel.send({
+						content:
+							"```json\n" +
+							JSON.stringify(error, null, 4) +
+							"\n```",
+					});
+
+					console.error(`Error executing ${event.name}`);
+					console.error(error);
+				}
+			} else {
+				//log date and time of event execution
+				console.log(
+					`${new Date().toLocaleString()} - [Event] ${event.name}`
+				);
+				client.on(event.name as string, (...args: []) =>
 					event.execute([...args])
 				);
-			} catch (error) {
-				const channel = client.guilds.cache
-					.get("713025650176294945")
-					.channels.cache.find(
-						(channel) => channel.name === "bot-log"
-					) as TextChannel;
-
-				channel.send({
-					content: `Error executing ${event.name}`,
-				});
-				channel.send({
-					content:
-						"```json\n" + JSON.stringify(error, null, 4) + "\n```",
-				});
-
-				console.error(`Error executing ${event.name}`);
-				console.error(error);
 			}
-		} else {
-			//log date and time of event execution
-			console.log(
-				`${new Date().toLocaleString()} - [Event] ${event.name}`
-			);
-			client.on(event.name as string, (...args: []) =>
-				event.execute([...args])
-			);
 		}
-	}
 
-	client.login(token);
+		client.login(token);
+}
+
+for (const token in config.tokens)
+{
+	startBot(token);
 }
