33f2404b55ba731319e9d67c9e0cb10ec6dc16ad
David0403 <48942146+Noboroto@users.noreply.github.com>
2023-11-14 21:59:46 +0700

    Update index.ts

--- root/src/index.ts
+++ root/src/index.ts
@@ -1,67 +1,54 @@
 // Require the necessary discord.js classes
 import { Client, GatewayIntentBits, TextChannel } from "discord.js";
 import events from "./events-bot/index";
-import config from "./configValues";
 
 // Create a new client instance
 
-const startBot = async(token) => {
-		const client = new Client({
-			intents: [
-				GatewayIntentBits.Guilds,
-				GatewayIntentBits.GuildMessages,
-				GatewayIntentBits.MessageContent,
-				GatewayIntentBits.GuildMembers,
-				GatewayIntentBits.GuildVoiceStates,
-				GatewayIntentBits.GuildMessageReactions,
-				GatewayIntentBits.GuildMessageTyping,
-				GatewayIntentBits.GuildPresences,
-				GatewayIntentBits.GuildEmojisAndStickers,
-				GatewayIntentBits.MessageContent,
-			],
-		});
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildVoiceStates,
+		GatewayIntentBits.GuildMessageReactions,
+		GatewayIntentBits.GuildMessageTyping,
+		GatewayIntentBits.GuildPresences,
+		GatewayIntentBits.GuildEmojisAndStickers,
+		GatewayIntentBits.MessageContent,
+	],
+});
 
-		for (const event of events) {
-			if (event.once) {
-				try {
-					client.once(event.name as string, (...args: []) =>
-						event.execute([...args])
-					);
-				} catch (error) {
-					const channel = client.guilds.cache
-						.get("713025650176294945")
-						.channels.cache.find(
-							(channel) => channel.name === "bot-log"
-						) as TextChannel;
+for (const event of events) {
+	if (event.once) {
+		try {
+			client.once(event.name as string, (...args: []) =>
+				event.execute([...args])
+			);
+		} catch (error) {
+			const channel = client.guilds.cache
+				.get("713025650176294945")
+				.channels.cache.find(
+					(channel) => channel.name === "bot-log"
+				) as TextChannel;
 
-					channel.send({
-						content: `Error executing ${event.name}`,
-					});
-					channel.send({
-						content:
-							"```json\n" +
-							JSON.stringify(error, null, 4) +
-							"\n```",
-					});
+			channel.send({
+				content: `Error executing ${event.name}`,
+			});
+			channel.send({
+				content: "```json\n" + JSON.stringify(error, null, 4) + "\n```",
+			});
 
-					console.error(`Error executing ${event.name}`);
-					console.error(error);
-				}
-			} else {
-				//log date and time of event execution
-				console.log(
-					`${new Date().toLocaleString()} - [Event] ${event.name}`
-				);
-				client.on(event.name as string, (...args: []) =>
-					event.execute([...args])
-				);
-			}
+			console.error(`Error executing ${event.name}`);
+			console.error(error);
 		}
-
-		client.login(token);
+	} else {
+		//log date and time of event execution
+		console.log(`${new Date().toLocaleString()} - [Event] ${event.name}`);
+		client.on(event.name as string, (...args: []) =>
+			event.execute([...args])
+		);
+	}
 }
 
-for (const token in config.tokens)
-{
-	startBot(token);
-}
+client.login(process.env.TOKEN);
