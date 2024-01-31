cfb59b5277a21686e8d3d46e5e9c8c23fbe2928a
David0403 <thanhtuvo135@gmail.com>
2024-01-31 23:12:36 +0700

    Update messageCreate.ts

--- root/src/events-bot/messageCreate.ts
+++ root/src/events-bot/messageCreate.ts
@@ -1,8 +1,23 @@
 import { Events, Message } from "discord.js";
 import fs from "fs";
 
-const dictionaryPath = "./files/reactionsDict.json";
-const reactionsDict = JSON.parse(fs.readFileSync(dictionaryPath, "utf8"));
+const reactionDictPath = "./files/reactionsDict.json";
+const reactionsDict = JSON.parse(fs.readFileSync(reactionDictPath, "utf8"));
+
+const reactToMessage = (message: Message) => {
+	for (const key in reactionsDict) {
+		if (message.content.toLowerCase().includes(key)) {
+			reactionsDict[key].forEach((reaction: string) => {
+				const emoji =
+					message.guild?.emojis.cache.find(
+						(emo) => emo.name === reaction
+					) ?? reaction;
+
+				message.react(emoji);
+			});
+		}
+	}
+}
 
 export default {
 	name: Events.MessageCreate,
@@ -10,17 +25,6 @@ export default {
 	async execute(args: Message[]) {
 		const message = args[0] as Message;
 
-		for (const key in reactionsDict) {
-			if (message.content.toLowerCase().includes(key)) {
-				reactionsDict[key].forEach((reaction: string) => {
-					const emoji =
-						message.guild?.emojis.cache.find(
-							(emo) => emo.name === reaction
-						) ?? reaction;
-
-					message.react(emoji);
-				});
-			}
-		}
+		reactToMessage(message);
 	},
 };
