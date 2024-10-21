d99208c4a3f9025b1ac1b66373c4d0e3ec4e51f7
David0403 <thanhtuvo135@gmail.com>
2024-10-22 00:26:32 +0700

    Add purge commands

---
+++ root/src/commands/purge.ts
@@ -0,0 +1,28 @@
+import { PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import anyCmd from "./purge/any";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("purge")
+    .setDescription("Purge messages in this channel.")
+
+    .addSubcommand(anyCmd.addCommand)
+    .setDMPermission(true)
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+  async execute(interaction) {
+    const commands = interaction.options.getSubcommand();
+    console.info(
+      `[[${interaction.client.user?.username}][Subcommad] ${interaction.user.username} - ${commands}`
+    );
+    switch (commands) {
+      case anyCmd.name:
+        await anyCmd.execute(interaction);
+        break;
+      default:
+        interaction.reply({
+          content: `Invalid subcommand ${commands}`,
+          ephemeral: true,
+        });
+    }
+  },
+};
