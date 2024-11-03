b0688b0a884131b9b5beb874f0d379583e9fe030
David0403 <thanhtuvo135@gmail.com>
2024-11-04 00:06:27 +0700

    Update messageUpdate.ts

--- root/src/events-bot/messageUpdate.ts
+++ root/src/events-bot/messageUpdate.ts
@@ -4,22 +4,45 @@ import fs from "fs";
 const reactionDictPath = "./files/reactionsDict.json";
 const reactionsDict = JSON.parse(fs.readFileSync(reactionDictPath, "utf8"));
 
+const extractReactEmojis = (text: string) => {
+  // Pattern với flag global (g) để lấy tất cả matches
+  const pattern =
+    /react\s+(?:\p{Emoji_Presentation}|\p{Extended_Pictographic}|<a?:.+?:\d{17,20}>)/giu;
+
+  // Lấy tất cả các matches
+  const matches = [...text.matchAll(pattern)];
+
+  // Pattern để tách riêng emoji từ mỗi match
+  const emojiPattern =
+    /(?:\p{Emoji_Presentation}|\p{Extended_Pictographic}|<a?:.+?:\d{17,20}>)/gu;
+
+  // Lấy emoji từ mỗi match và lọc null
+  return matches
+    .map((match) => match[0].match(emojiPattern)?.[0])
+    .filter((emoji) => emoji !== undefined) as string[];
+};
+
+const reactToMessage = (message: Message) => {
+  const emojisStr: String[] = extractReactEmojis(message.content);
+  for (const key in reactionsDict) {
+    if (message.content.toLowerCase().includes(key)) {
+      reactionsDict[key].forEach((reaction: string) => {
+        emojisStr.push(reaction);
+      });
+    }
+  }
+  emojisStr.forEach((str: string) => {
+    const emoji =
+      message.guild?.emojis.cache.find((emo) => emo.name === str) ?? str;
+    message.react(emoji);
+  });
+};
+
 export default {
   name: Events.MessageUpdate,
   once: false,
   async execute(args: Message[]) {
     const message = args[1] as Message;
-
-    for (const key in reactionsDict) {
-      if (message.content.toLowerCase().includes(key)) {
-        reactionsDict[key].forEach((reaction: string) => {
-          const emoji =
-            message.guild?.emojis.cache.find((emo) => emo.name === reaction) ??
-            reaction;
-
-          message.react(emoji);
-        });
-      }
-    }
+    reactToMessage(message);
   },
 };
