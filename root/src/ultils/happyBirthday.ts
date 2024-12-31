01c2e70db3a23585c5aeb71d6d22baad31e83f77
David0403 <thanhtuvo135@gmail.com>
2025-01-01 00:36:35 +0700

    Update happyBirthday.ts

--- root/src/ultils/happyBirthday.ts
+++ root/src/ultils/happyBirthday.ts
@@ -103,13 +103,16 @@ export const getWishID = async (guid: string): Promise<String> => {
   const guildObj = await birthdayDb.get(guid);
 
   if (guildObj === undefined || guildObj === null) {
+		console.info("Reset wishes because guildObj is null");
     await resetWishes(guid);
   }
   if ((await birthdayDb.get(`${guid}.count`)) === 0) {
+		console.info("Reset wishes because count is 0");
     await resetWishes(guid);
   }
   let maxMsg = await birthdayDb.get(`max`);
   if (maxMsg === null || maxMsg === undefined || maxMsg === 0) {
+		console.info("Reset wishes because maxMsg is 0");
     await resetWishes(guid);
     maxMsg = await birthdayDb.get(`max`);
   }
